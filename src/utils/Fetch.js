a959aceb122e36cf92e0a682820b9aaf35b03ed5
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-10-13 03:40:29 +0100

    format code

--- src/utils/Fetch.js
+++ src/utils/Fetch.js
@@ -1,48 +1,31 @@
-
-
-import react, { useState, useEffect } from 'react';
-import RenderCities from '../components/cities';
+import react, { useState, useEffect } from "react";
+import RenderCities from "../components/cities";
 
 export function FetchData() {
-
-    const [isLoading, setIsLoading] = useState(true);
-    const [resData, setResData] = useState({});
-    useEffect(() => {
-      fetch(`https://api.openaq.org/v1/cities?country=GB`)
-      .then(response => response.json())
-      .then(response => {
-     
-     
-  
-        setResData(response.results)
+  const [isLoading, setIsLoading] = useState(true);
+  const [resData, setResData] = useState({});
+  useEffect(() => {
+    fetch(`https://api.openaq.org/v1/cities?country=GB`)
+      .then((response) => response.json())
+      .then((response) => {
+        setResData(response.results);
         setIsLoading(false);
-  
-      })
-    }, []);
-   
-  
-  
-  
-  
-  
-    return (
-      <>
+      });
+  }, []);
+
+  return (
+    <>
       {isLoading && "loading"}
-      
-      { !isLoading &&
-      <div>
 
-  
-      <div
-       >{resData.map(cities => {
-        return(<RenderCities cityData = {cities} />);
-
-      
-      })}</div>
-        
+      {!isLoading && (
+        <div>
+          <div>
+            {resData.map((cities) => {
+              return <RenderCities cityData={cities} />;
+            })}
+          </div>
         </div>
-      }
-  </>
-  
-    );
+      )}
+    </>
+  );
 }
